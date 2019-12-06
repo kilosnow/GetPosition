8f35349d6054050e88b46f6f55701a306e1e7dc1
Liu Ying <kilosnow@live.com>
2019-12-06 14:29:06 +0800

    添加项目文件。

---
+++ GetPosition/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace GetPosition
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timerRun = new System.Windows.Forms.Timer(this.components);
+            this.labelX = new System.Windows.Forms.Label();
+            this.labelY = new System.Windows.Forms.Label();
+            this.buttonAbout = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.labelColor = new System.Windows.Forms.Label();
+            this.pictureColor = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureColor)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timerRun
+            // 
+            this.timerRun.Enabled = true;
+            this.timerRun.Tick += new System.EventHandler(this.timerRun_Tick);
+            // 
+            // labelX
+            // 
+            this.labelX.AutoSize = true;
+            this.labelX.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelX.Location = new System.Drawing.Point(16, 16);
+            this.labelX.Name = "labelX";
+            this.labelX.Size = new System.Drawing.Size(123, 25);
+            this.labelX.TabIndex = 0;
+            this.labelX.Text = "坐标 X: 9999";
+            // 
+            // labelY
+            // 
+            this.labelY.AutoSize = true;
+            this.labelY.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelY.Location = new System.Drawing.Point(152, 16);
+            this.labelY.Name = "labelY";
+            this.labelY.Size = new System.Drawing.Size(122, 25);
+            this.labelY.TabIndex = 1;
+            this.labelY.Text = "坐标 Y: 9999";
+            // 
+            // buttonAbout
+            // 
+            this.buttonAbout.BackColor = System.Drawing.Color.LimeGreen;
+            this.buttonAbout.ForeColor = System.Drawing.Color.White;
+            this.buttonAbout.Location = new System.Drawing.Point(120, 96);
+            this.buttonAbout.Name = "buttonAbout";
+            this.buttonAbout.Size = new System.Drawing.Size(75, 23);
+            this.buttonAbout.TabIndex = 5;
+            this.buttonAbout.Text = "关于";
+            this.buttonAbout.UseVisualStyleBackColor = false;
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.BackColor = System.Drawing.Color.Red;
+            this.buttonExit.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonExit.ForeColor = System.Drawing.Color.White;
+            this.buttonExit.Location = new System.Drawing.Point(208, 96);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(75, 23);
+            this.buttonExit.TabIndex = 3;
+            this.buttonExit.Text = "退出";
+            this.buttonExit.UseVisualStyleBackColor = false;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // labelColor
+            // 
+            this.labelColor.AutoSize = true;
+            this.labelColor.Font = new System.Drawing.Font("微软雅黑", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.labelColor.Location = new System.Drawing.Point(16, 56);
+            this.labelColor.Name = "labelColor";
+            this.labelColor.Size = new System.Drawing.Size(75, 25);
+            this.labelColor.TabIndex = 4;
+            this.labelColor.Text = "label C";
+            // 
+            // pictureColor
+            // 
+            this.pictureColor.Location = new System.Drawing.Point(232, 64);
+            this.pictureColor.Name = "pictureColor";
+            this.pictureColor.Size = new System.Drawing.Size(24, 24);
+            this.pictureColor.TabIndex = 6;
+            this.pictureColor.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.buttonExit;
+            this.ClientSize = new System.Drawing.Size(289, 121);
+            this.Controls.Add(this.pictureColor);
+            this.Controls.Add(this.labelColor);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonAbout);
+            this.Controls.Add(this.labelY);
+            this.Controls.Add(this.labelX);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "☆☆ 得到→点→色 ☆☆";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureColor)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timerRun;
+        private System.Windows.Forms.Label labelX;
+        private System.Windows.Forms.Label labelY;
+        private System.Windows.Forms.Button buttonAbout;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Label labelColor;
+        private System.Windows.Forms.PictureBox pictureColor;
+    }
+}
+
---
+++ GetPosition/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace GetPosition
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetDC(IntPtr hwnd);
+
+        [DllImport("user32.dll")]
+        public static extern Int32 ReleaseDC(IntPtr hwnd, IntPtr hdc);
+
+        [DllImport("gdi32.dll")]
+        public static extern uint GetPixel(IntPtr hdc, int nXPos, int nYPos);
+
+        Point mp;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void timerRun_Tick(object sender, EventArgs e)
+        {
+            mp = Control.MousePosition;
+            labelX.Text = string.Format("坐标 X: {0}", mp.X.ToString());
+            labelY.Text = string.Format("坐标 Y: {0}", mp.Y.ToString());
+            Color c = getColor();
+            labelColor.Text = "颜色: " + c.R.ToString() + c.G.ToString() + c.B.ToString();
+            pictureColor.BackColor = c;
+
+        }
+
+        public Color getColor()
+        {
+            //int h = WindowFromPoint(x, y);
+            IntPtr hdc = GetDC(IntPtr.Zero);
+
+            int c =(int)GetPixel(hdc, mp.X, mp.Y);
+            return Color.FromArgb(c);
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
